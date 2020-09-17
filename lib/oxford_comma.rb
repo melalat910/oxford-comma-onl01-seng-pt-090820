36f3ddab93abb0d59804e4996aad7aea217c4f03
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-17 07:53:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
+  return array.first if array.size == 1
 
-    if array.length < 2
-  end 	    array.join
-    elsif array.length == 2
-      array.insert(1, "and").join(" ")
-    else
-      str = ", and " + array.pop
-      str = array.join(", ") << str
-    end
+
+  if array.size == 2
+    array.insert(1, "and").join(" ")
+  elsif array.size > 2
+    array[-1].insert(0, "and ")
+    array.join(", ")
   end
