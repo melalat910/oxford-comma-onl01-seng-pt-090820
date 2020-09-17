4e4bf79ad1f63189e38dcaa7b99fa735e7dcb117
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-17 07:51:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
 
-end
+    if array.length < 2
+  end 	    array.join
+    elsif array.length == 2
+      array.insert(1, "and").join(" ")
+    else
+      str = ", and " + array.pop
+      str = array.join(", ") << str
+    end
+  end
